a1103ac63c8a6cbf4dd890597569ae29804f528e
Ismayil9918 <ismayilbi@code.edu.az>
2022-03-01 07:25:15 +0400

    Home work

---
+++ Homwork2022/Extension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homwork2022
+{
+    static class Extension
+    {
+        public static string isCapitalized(this string word)
+        {
+            word = word.ToLower();
+
+
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(char.ToUpper(word[0]).ToString());
+
+            for (int i = 1; i < word.Length; i++) 
+            {
+                stringBuilder.Append(word[i]);
+            }
+
+            return stringBuilder.ToString();
+
+        }
+    }
+}
---
+++ Homwork2022/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Homwork2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "exTensiON claSSi staTIc OLMalIdiR";
+            Console.WriteLine(name.isCapitalized());
+        }
+    }
+}
